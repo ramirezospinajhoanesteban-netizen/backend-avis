1ac46feca2a5670a0149b4ebc110ec4990c4bb5b
Jhoan Ramirez <ramirezospinajhoanesteban@gmail.com>
2026-04-30 11:40:24 -0500

    contenedor listo

--- config/cors.php
+++ config/cors.php
@@ -5,10 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',   // Vite dev
-        'http://127.0.0.1:5173',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
